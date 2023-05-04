99c5e18705b9a62330d9e273e20436f1c826a3b5
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-05-03 20:59:00 -0700

    stufrf ast

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,12 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -23,6 +25,35 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		//amqp emsaging protocol
+		fileSet := token.NewFileSet()
+		astre, err := parser.ParseFile(fileSet, args[0], nil, parser.ParseComments)
+		if err != nil {
+			panic(err)
+		}
+		//macro edit ast to do actions extend functionality
+		pacName := astre.Name.Name
+		deps := astre.Imports
+		comments := astre.Comments
+		exported := map[string]int{"Variable": 0, "Function": 0}
+		ast.Inspect(astre, func(n ast.Node) bool {
+			toFind, okay := n.(*ast.Ident) //ast node of all types
+			if okay {
+				if toFind.IsExported() {
+					switch toFind.Obj.Kind {
+					case ast.Var:
+						exported["Variable"]++
+					case ast.Fun:
+						exported["Function"]++
+					}
+				}
+			} else {
+				return false
+			}
+			//st extend asty to make amcros or travers eto fgind nodes of each part of code experssions
+			return true
+
+		})
 
 	},
 }
@@ -47,5 +78,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
